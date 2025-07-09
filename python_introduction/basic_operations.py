311808f7a7d31b4f45fae1e77829d9af30cd7b9f
joseph aderibigbe <aderibigbejoseph131@gmail.com>
2025-07-09 17:18:34 +0100

    Add basic_operations.py with math operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#Assigning the values 
+number1 = 10
+number2 = 5
+
+#Calculating the math 
+addition = number1 + number2 
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+#Printing the results 
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
